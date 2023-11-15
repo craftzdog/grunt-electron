3a23290790f90df9d1b65b19cbfb19a431eff90f
Takuya Matsuyama <hi@craftz.dog>
2023-11-15 14:28:00 +0900

    chore(dep): bump up stack

--- gruntfile.js
+++ gruntfile.js
@@ -1,34 +1,29 @@
-'use strict';
-module.exports = grunt => {
+"use strict";
+module.exports = (grunt) => {
 	grunt.initConfig({
 		electron: {
 			package: {
 				options: {
-					name: 'Fixture',
-					dir: 'test/fixture',
-					out: 'test/tmp',
-					electronVersion: '1.3.5',
-					platform: 'darwin',
-					arch: 'x64'
-				}
-			}
+					name: "Fixture",
+					dir: "test/fixture",
+					out: "test/tmp",
+					electronVersion: "25.1.1",
+					platform: "darwin",
+					arch: "x64",
+				},
+			},
 		},
 		nodeunit: {
-			tasks: ['test/test.js']
+			tasks: ["test/test.js"],
 		},
 		clean: {
-			test: ['test/tmp/**']
-		}
+			test: ["test/tmp/**"],
+		},
 	});
 
-	grunt.loadTasks('tasks');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadTasks("tasks");
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-nodeunit");
 
-	grunt.registerTask('default', [
-		'clean',
-		'electron',
-		'nodeunit',
-		'clean'
-	]);
+	grunt.registerTask("default", ["clean", "electron", "nodeunit", "clean"]);
 };
