45e652db752493dd4a604d0e1a0938e5333f23cc
Takuya Matsuyama <hi@craftz.dog>
2023-11-15 14:33:13 +0900

    feat(electron): use the latest electron packager

--- tasks/electron.js
+++ tasks/electron.js
@@ -1,20 +1,22 @@
-'use strict';
-const electronPackager = require('electron-packager');
+"use strict";
+const electronPackager = require("@electron/packager");
 
-module.exports = grunt => {
+module.exports = (grunt) => {
 	grunt.registerMultiTask(
-		'electron',
-		'Package Electron apps',
+		"electron",
+		"Package Electron apps",
 		async function () {
 			const done = this.async();
 
 			try {
-				await electronPackager(this.options());
+				const appPaths = await electronPackager(this.options());
+
+				console.log(`Electron app bundles created:\n${appPaths.join("\n")}`);
 				done();
 			} catch (error) {
 				grunt.warn(error);
 				done();
 			}
-		}
+		},
 	);
 };
